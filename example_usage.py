826e820ff3670ee7d1133346280c9846b7c7d188
KOSASIH <kosasihg88@gmail.com>
2023-10-08 18:39:59 +0700

    Create example_usage.py

---
+++ example_usage.py
@@ -0,0 +1,7 @@
+# Example usage
+mass = 100  # metric tons
+velocity = 0.1  # kilometers per second
+efficiency = 0.9  # 90% efficiency
+
+required_power = calculate_fusion_power(mass, velocity, efficiency)
+print(f"The required fusion power for interstellar travel is {required_power} watts.")
